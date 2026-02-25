c880105b2a517937cc0d2c64d2317b074f9e7b39
issamlammri <issam.lammri@live-session.fr>
2026-02-25 17:24:34 +0100

    passer en URL relative.

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_HOST || 'http://localhost:8090/api',
+  baseURL: import.meta.env.VITE_API_HOST || '/api',
 });
 
 apiClient.interceptors.request.use((config) => {
